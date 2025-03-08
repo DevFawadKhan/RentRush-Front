6f91d376bf56d4b4f98a0af55cac32889aab9376
Foxy__design <102384492+syedafatima26@users.noreply.github.com>
2025-03-08 17:07:29 +0500

    Update Variety.jsx

--- src/components/Variety.jsx
+++ src/components/Variety.jsx
@@ -3,17 +3,12 @@ import TestContainer from "./TestContainer";
 
 function Variety() {
   return (
-    <div className="relative z-10 h-auto lg:h-screen bg-[#2C2C2C] flex flex-col justify-center items-center" id="rent">
-      <div className="h-auto relative bg-[#2C2C2C] w-full py-16">
-        <h1 className="text-white font-bold text-2xl lg:text-3xl w-[100%] text-center mx-auto pt-8">
-          Trusted by Thousands of Happy Customers
-        </h1>
-        <p className="w-[100%] mx-auto py-3 text-white text-center">
-          “RentRush is proud to be trusted by thousands of satisfied customers,
-          ensuring quality and reliability in every rental.”
-        </p>
-        <TestContainer />
-      </div>
+    <div className="bg-white py-12">
+      <h1 className="text-3xl text-center font-bold">Trusted by Thousands of Happy Customers</h1>
+      <p className="text-lg text-center mt-2">
+        “RentRush is proud to be trusted by thousands of satisfied customers, ensuring quality and reliability in every rental.”
+      </p>
+      <TestContainer />
     </div>
   );
 }
