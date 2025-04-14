102ae486cfc2b78339c268ace25b63fa0038d7a3
syedafatima26 <syedafatimamansoor@gmail.com>
2025-04-14 23:10:41 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,86 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/login.jsx";
-import SignUp from "./components/signup.jsx";
-import ShowroomSignUp from "./components/showroom/signup.jsx"
-import ShowroomDashboard from "./components/showroom/dashboard.jsx";
-import ShowroomInventory from "./components/showroom/inventory.jsx"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+// Public Components
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import LandingPage from "./components/landingPage.jsx";
-import CarsDashboard from "./components/customer/Dashboard.jsx";
-import UserProfile from "./components/customer/profile.jsx";
-import Cars from  "./components/customer/cars.jsx";
-import Showrooms from "./components/customer/showrooms.jsx";
-import Bookings from "./components/customer/bookings.jsx";
-import Adminpage from "./components/admin/Adminpage.jsx";
-import ProtectedLayout from "./auth/protectedRoute.jsx";
-import EditBookingModal from '../src/components/customer/EditBooking.jsx'
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import ResetConfirmation from "./components/ResetConfirmation";
+
+// Customer Components
+import CustomerDashboard from "./components/customer/Dashboard";
+import UserProfile from "./components/customer/Profile";
+import Cars from "./components/customer/Cars";
+import Showrooms from "./components/customer/Showrooms";
+import Bookings from "./components/customer/Bookings";
+import EditBooking from "./components/customer/EditBooking";
 import CarDetailsScreen from '../src/components/customer/CarDetailsScreen.jsx'
-import Invoice from "../src/components/customer/invoice.jsx"
-import ForgotPassword from "./components/ForgotPassword.jsx";
-import ResetPassword from "./components/ResetPassword.jsx";
-import ResetConfirmation from "./components/ResetConfirmation.jsx";
-import Maintenance from "./components/showroom/maintenance.jsx";
-import Services from "./components/customer/Services.jsx";
-import Reviews from "./components/customer/Reviews.jsx";
+import Invoice from "./components/customer/Invoice";
+import Services from "./components/customer/Services";
+import Reviews from "./components/customer/Reviews";
 import Showroomcars from "./components/customer/Showroomcars.jsx";
+
+// Showroom Components
+import ShowroomSignUp from "./components/showroom/SignUp";
+import ShowroomDashboard from "./components/showroom/Dashboard";
+import ShowroomInventory from "./components/showroom/Inventory";
+import ShowroomMaintenance from "./components/showroom/Maintenance";
+
+// Admin Components
+import Adminpage from "./components/admin/Adminpage.jsx";
+
+
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/showroom/signup" element={<ShowroomSignUp />}></Route>
-          <Route path="/" element={<LandingPage />}></Route>
-          <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-        <Route path="/reset-password/:token" element={<ResetPassword />}></Route>
-        <Route path="/reset-confirmation" element={<ResetConfirmation />}></Route>
-        <Route path="/showroom/maintenance" element={<Maintenance />}></Route>
+   
+        <BrowserRouter>
+          <Routes>
+            {/* Public Routes */}
+            <Route>
+              <Route index element={<LandingPage />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="reset-password/:token" element={<ResetPassword />} />
+              <Route path="reset-confirmation" element={<ResetConfirmation />} />
+              <Route path="showroom/signup" element={<ShowroomSignUp />} />
+            </Route>
 
-          {/* Protected Routes */}
-          <Route element={<ProtectedLayout />}>
-            <Route
-             path="/showroom/Dashboard"
-              element={<ShowroomDashboard />}
-            ></Route>
-            <Route
-              path="/showroom/inventory"
-              element={<ShowroomInventory />}
-            ></Route>
-            <Route
-              path="/customer/Dashboard"
-              element={<CarsDashboard />}
-            ></Route>
-            <Route path="/customer/profile" element={<UserProfile />}></Route>
-            <Route path="/customer/cars" element={<Cars />}></Route>
-            <Route path="/customer/Showrooms" element={<Showrooms />}></Route>
-            <Route path="/customer/bookings" element={<Bookings />}></Route>
+            {/* Customer Routes */}
+            <Route >
+              <Route index element={<CustomerDashboard />} />
+              <Route path="dashboard" element={<CustomerDashboard />} />
+              <Route path="profile" element={<UserProfile />} />
+              <Route path="cars" element={<Cars />} />
+              <Route path="showrooms" element={<Showrooms />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="edit-booking" element={<EditBooking />} />
+              <Route path="/customer/CarDetailsScreen/:bookingId" element={<CarDetailsScreen></CarDetailsScreen>}></Route>
+              <Route path="invoice" element={<Invoice />} />
+              <Route path="services" element={<Services />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="/customer/detailcars/:id" element={<Showroomcars/>}></Route> 
+            </Route>
+
+            {/* Showroom Routes */}
+            <Route   >
+              <Route index element={<ShowroomDashboard />} />
+              <Route path="dashboard" element={<ShowroomDashboard />} />
+              <Route path="inventory" element={<ShowroomInventory />} />
+              <Route path="maintenance" element={<ShowroomMaintenance />} />
+            </Route>
+
+            {/* Admin Routes */}
+            <Route   >
             <Route path="/admin/Adminpage" element={<Adminpage />}></Route>
-            <Route path="/customer/editbooking" element={<EditBookingModal></EditBookingModal>}></Route>
-            <Route path="/customer/CarDetailsScreen/:bookingId" element={<CarDetailsScreen></CarDetailsScreen>}></Route>
-            <Route path="/customer/invoice" element={<Invoice></Invoice>}></Route>
-            <Route path="/customer/services" element={<Services/>}></Route>
-            <Route path="/customer/reviews" element={<Reviews/>}></Route> 
-            <Route path="/customer/detailcars/:id" element={<Showroomcars/>}></Route> 
-            
-            
-            
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+            </Route>
+
+            {/* 404 Page */}
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </BrowserRouter>
   );
 }
 
