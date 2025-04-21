61b9773d5e6877456bea565edf45346992a96f25
syedafatima26 <syedafatimamansoor@gmail.com>
2025-04-21 23:23:26 +0500

    update

--- src/components/showroom/inventory.jsx
+++ src/components/showroom/inventory.jsx
@@ -222,7 +222,7 @@ function ShowroomInventory() {
                       {vehicle.year}
                     </td>
                     <td className="px-4 py-2 border-b border-gray-700">
-                      {vehicle.rentRate} RS/hr
+                      {vehicle.rentRate} RS/Day
                     </td>
                     <td className="px-4 py-2 border-b border-gray-700">
                       {vehicle.color}
